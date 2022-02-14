fb1236778597148d1ef747d8bda3f121b422218b
Lee Hye-In <59090549+hyeinlee725@users.noreply.github.com>
2022-02-14 19:21:59 +0900

    mux_assign - Verilog Code

---
+++ mux_assign/mux_assign.v
@@ -0,0 +1,8 @@
+module mux_assign ( input a, 
+						  input b, 
+						  input c, 
+						  input d, 
+						  input s0, s1,
+						  output out); 
+	assign out = s1 ? (s0 ? d : c) : (s0 ? b : a); 
+endmodule
